b3c5d9bc546ce1d50900e698ca90995de811cd0b
Sebastian Arias Usma <sebasarias33314@gmail.com>
2023-10-13 19:19:15 -0500

    Primer commit telepong

---
+++ Server.c
@@ -0,0 +1,272 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <pthread.h>
+
+#define BUFFER_SIZE 1024
+
+typedef struct {
+    int dir_x;
+    int dir_y;
+    int x;
+    int y;
+    int score_1;
+    int score_2;
+} Ball;
+
+typedef struct {
+    int dir_y;
+    int x;
+    int y;
+} PlayerSkin;
+
+typedef struct{
+    int sockfd;
+    struct sockaddr_in serverAddr, clientAddr, clientAddrs[2];
+    int addr_size;
+} GameThreadArgs;
+
+FILE *logfd;
+
+
+void ballPhysics(Ball* ball);
+void reset(Ball* ball);
+void ballBounce(Ball* ball);
+
+void playerPhysics(PlayerSkin* player);
+void hit(PlayerSkin* player, Ball* ball, int Player_1);
+
+int find_client_index(struct sockaddr_in *clientAddrs, struct sockaddr_in client);
+void log_info(const char *message);
+void log_error(const char *message);
+
+void *handle_game(void *arg);
+
+int main(int argc, char *argv[]);
+
+
+void ballPhysics(Ball* ball) {
+    ball->x += ball->dir_x;
+    ball->y += ball->dir_y;
+}
+
+void reset(Ball* ball) {
+    srand(time(NULL));
+    ball->x = 380;
+    ball->y = 280;
+    ball->dir_x = -(ball->dir_x);
+    ball->dir_y = (rand() % 2) ? -2 : 2;
+}
+
+void ballBounce(Ball* ball) {
+    if (ball->x <= -40) {
+        reset(ball);
+        ball->score_2 += 1;
+    }
+    if (ball->x >= 800) {
+        reset(ball);
+        ball->score_1 += 1;
+    }
+    if (ball->y <= 0) {
+        ball->dir_y = -(ball->dir_y);
+    }
+    if (ball->y + 40 >= 600) {
+        ball->dir_y = -(ball->dir_y);
+    }
+}
+
+void playerPhisics(PlayerSkin* player) {
+    player->y += player->dir_y;
+    if (player->y <= 0) {
+        player->y = 0;
+    }
+    if (player->y + 100 >= 600) {
+        player->y = 600 - 100;
+    }
+}
+
+void hit(PlayerSkin* player, Ball* ball, int Player_1) {
+    if (ball->x < player->x + 22 && ball->x > player->x && ball->y + 40 > player->y && ball->y < player->y + 100) {
+        ball->dir_x = -ball->dir_x;
+        ball->x = Player_1 ? (player->x + 22) : (player->x - 40);
+    }
+}
+
+int find_client_index(struct sockaddr_in *clientAddrs, struct sockaddr_in client) {
+    for (int i = 0; i < 2; i++) {
+        if (clientAddrs[i].sin_addr.s_addr == client.sin_addr.s_addr && clientAddrs[i].sin_port == client.sin_port) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void log_info(const char *message){
+    fprintf(logfd, "[INFO] %s\n", message);
+    fflush(logfd);
+}
+
+void log_error(const char *message){
+    fprintf(logfd, "[ERROR] %s\n", message);
+    fflush(logfd);
+}
+
+void *handle_game(void *arg)
+{
+    GameThreadArgs *args = (GameThreadArgs *)arg;
+    char start_msg[] = "START";
+    for (int i = 0; i < 2; i++)
+    {
+        sendto(args->sockfd, start_msg, strlen(start_msg), 0, (struct sockaddr *)&args->clientAddrs[i], args->addr_size);
+    }
+
+    printf("Juego comenzando...\n");
+    log_info("Juego comenzando\n");
+
+    Ball ball;
+
+    ball.x = 380;
+    ball.y = 280;
+
+    int random_number = rand() % 2;
+    ball.dir_x = (random_number == 0) ? -2 : 2;
+    random_number = rand() % 2;
+    ball.dir_y = (random_number == 0) ? -2 : 2;
+
+    ball.score_1 = 0;
+    ball.score_2 = 0;
+
+    PlayerSkin player_1, player_2;
+
+    player_1.x = 60;
+    player_1.y = 250;
+    player_1.dir_y = 0;
+
+    player_2.x = 718;
+    player_2.y = 250;
+    player_2.dir_y = 0;
+
+    char buffer[BUFFER_SIZE];
+
+    while (1)
+    {
+        int bytesReceived = recvfrom(args->sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&args->clientAddr, &args->addr_size);
+        if (bytesReceived > 0)
+        {
+            buffer[bytesReceived] = '\0';
+
+            int sendingClientIndex = find_client_index(args->clientAddrs, args->clientAddr);
+            char logMessage[256];
+            sprintf(logMessage, "El jugaror %d está: %d.\n", sendingClientIndex, atoi(buffer));
+            printf(logMessage);
+            log_info(logMessage);
+
+            if (sendingClientIndex == 0)
+            {
+                player_1.dir_y = atoi(buffer);
+            }
+            else if (sendingClientIndex == 1)
+            {
+                player_2.dir_y = atoi(buffer);
+            }
+
+            ballPhysics(&ball);
+            ballBounce(&ball);
+            playerPhisics(&player_1);
+            playerPhisics(&player_2);
+            hit(&player_1, &ball, 1);
+            hit(&player_2, &ball, 0);
+
+            for (int clientIndex = 0; clientIndex < 2; clientIndex++)
+            {
+                char tempBuffer2[256];
+                sprintf(tempBuffer2, "%d %d %d %d %d %d", ball.x, ball.y, player_1.y, player_2.y, ball.score_1, ball.score_2);
+                sendto(args->sockfd, tempBuffer2, strlen(tempBuffer2), 0, (struct sockaddr *)&args->clientAddrs[clientIndex], args->addr_size);
+            }
+        }
+    }
+}
+
+
+
+int main(int argc, char *argv[]) {
+    if (argc != 3){
+        fprintf(stderr, "Uso: %s <PORT> <Log File>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    int port = atoi(argv[1]);
+    char *logFile = argv[2];
+
+    logfd = fopen(logFile, "a");
+    if (logfd == NULL)
+    {
+        perror("Error al abrir el archivo de log");
+        exit(EXIT_FAILURE);
+    }
+
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0)
+    {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    if (bind(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+    {
+        perror("Error al enlazar");
+        close(sockfd);
+        exit(1);
+    }
+
+    struct sockaddr_in clientAddr;
+    socklen_t addr_size = sizeof(clientAddr);
+    struct sockaddr_in clientAddrs[2];
+
+    char buffer[BUFFER_SIZE];
+
+    while (1)
+    {
+        int connected_clients = 0;
+        while (connected_clients < 2)
+        {
+            int bytesReceived = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *)&clientAddr, &addr_size);
+            if (bytesReceived > 0)
+            {
+                buffer[bytesReceived] = '\0';
+                if (strcmp(buffer, "CONNECT") == 0 && find_client_index(clientAddrs, clientAddr) == -1)
+                {
+                    clientAddrs[connected_clients] = clientAddr;
+                    connected_clients++;
+                    printf("%d", connected_clients);
+                    printf("Cliente %d conectado desde %s:%d\n", connected_clients, inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+                    char log_message[256];
+                    sprintf(log_message, "Cliente %d conectado desde %s:%d", connected_clients, inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+                    log_info(log_message);
+                }
+            }
+        }
+
+        if (connected_clients == 2){
+            pthread_t game_thread;
+            GameThreadArgs *args = malloc(sizeof(GameThreadArgs));
+            memcpy(args->clientAddrs, clientAddrs, sizeof(clientAddrs));
+            args->sockfd = sockfd;
+            args->addr_size = sizeof(clientAddrs[0]);
+            pthread_create(&game_thread, NULL, handle_game, (void *)args);
+            pthread_detach(game_thread);
+        }
+    }
+    close(sockfd);
+
+    return 0;
+}
---
+++ game.py
@@ -0,0 +1,151 @@
+import socket
+import threading
+import pygame
+from pygame.locals import QUIT
+import socket
+
+FPS = 60
+WHITE = (255, 255, 255)
+BLACK = (0,0,0)
+
+SERVER_IP = '3.93.212.75'
+SERVER_PORT = 8080
+BUFFER_SIZE = 1024
+
+start_game = False
+score_player1 = 0
+score_player2 = 0
+running = True
+
+class ball:
+    def __init__(self, img):
+        self.img = pygame.image.load(img).convert_alpha()
+        self.x = 380
+        self.y = 280
+
+class playerSkin:
+    def __init__(self, img):
+        self.img = pygame.image.load(img).convert_alpha()
+        self.x = 0
+        self.y = 250
+        self.dir_y = 0
+        
+
+pygame.init() #Inicia el juego
+window = pygame.display.set_mode((800, 600)) #Dimension de la pantalla
+pygame.display.set_caption("TelePong Champions Edition") 
+
+background_pic = pygame.image.load("background.png").convert()  #Fondo del juego
+
+ball = ball("ball.png") #Creacion de la pelota
+player_1 = playerSkin("player1.png")  # Creacion del jugador
+player_1.x = 60  #Posición en x del jugador 1
+player_2 = playerSkin("player2.png") # Creacion del jugador
+player_2.x = 718 #Posición en x del jugador 2
+
+def refresh_screen_and_positions():
+    font = pygame.font.Font(None, 60) #Letras del juego
+    window.blit(background_pic, (0, 0))  # Fondo de pantalla seteada
+    
+    window.blit(ball.img, (ball.x, ball.y)) #Pintar la imagen con sus propiedades
+    window.blit(player_1.img, (player_1.x, player_1.y)) #pintar el jugador 1 con sus propiedades
+    window.blit(player_2.img, (player_2.x, player_2.y)) #pintar el jugador 2 con sus propiedades
+    txt = f"Player 1 {score_player1} : {score_player2} Player 2" #Pintar el marcador del juego
+    signal = font.render(txt, False, WHITE)
+    window.blit(signal, (800 / 2 - font.size(txt)[0] / 2, 50))
+    
+    pygame.display.flip()
+
+def listen_for_messages(client_socket): #Metodo que escucha del servidor y nos trae los datos para decodificarlos y mostrarlos
+    global start_game, score_player1, score_player2
+    while True:
+        data, _ = client_socket.recvfrom(BUFFER_SIZE)
+        if data:
+            data = data.decode()
+            if data == "START":
+                start_game = True
+                print("Los represnetantes de los equipos han llegado.")
+                continue
+            print("Posiciones: " + data)
+            data = data.split()
+            ball.x, ball.y, player_1.y, player_2.y, score_player1, score_player2 = map(int, data)
+
+            if not running:
+                break
+            refresh_screen_and_positions()
+
+def alerts(alert, y_offset=0): #Se usa para cuando se inicia el juego y mostrar los mensajes que seteamos antes 
+    font = pygame.font.Font(None, 60)
+    text_surface = font.render(alert, True, BLACK)
+    text_rect = text_surface.get_rect(center=(400, 300 + y_offset))
+    window.blit(text_surface, text_rect)
+    pygame.display.flip()
+
+def main():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    client_socket.sendto("CONNECT".encode(), (SERVER_IP, SERVER_PORT)) #Le mandamos al servidor la IP y el puerto
+    print("Se ha conectado al servidor.")
+    
+    listener_thread = threading.Thread(target=listen_for_messages, args=(client_socket,))
+    listener_thread.daemon = True
+    listener_thread.start()
+    
+    window.fill(WHITE)
+    alerts("Bienvenido a Pong Champions Edition!", -20)
+    alerts("Esperando otro jugador...", 20)
+    while not start_game:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return
+
+    global running
+    clock = pygame.time.Clock()
+    current_movement = "0"
+
+    while running:
+        refresh_screen_and_positions()
+        if (score_player1 == 5 or score_player2 == 5):
+            running = False
+            
+        for event in pygame.event.get():
+            if (event.type == QUIT):
+                running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    current_movement = "-5"
+                if event.key == pygame.K_s:
+                    current_movement = "5"
+
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w or event.key == pygame.K_s:
+                    current_movement = "0"
+
+        client_socket.sendto(current_movement.encode(), (SERVER_IP, SERVER_PORT))
+            
+        pygame.display.flip() #Actualiza la pantalla
+        clock.tick(FPS)
+        
+    window.fill(WHITE)
+    if score_player1 == 5:
+        alerts("El jugador 1 ha ganado", -20)
+        pygame.time.wait(2000)
+    elif score_player2 == 5:
+        alerts("El jugador 2 ha ganado!", -20)
+        pygame.time.wait(2000)
+    else:
+        alerts("Cerrando en 5 segundos...", 20)
+        pygame.time.wait(1000)
+        
+    for i in range(5):
+            window.fill(WHITE)
+            alerts(f"Juego terminando en {5-i}", 20)
+            pygame.time.wait(1000)
+
+    pygame.quit()
+    client_socket.close()
+
+if __name__ == "__main__":
+    main()
